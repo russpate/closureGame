98459fe1a4f25fd288f0e78014269a792c262fc8
Russell Pate <russ@russpate.com>
2016-03-12 11:08:54 -0500

    fixing randomize

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ function Character(options){
     console.log("the", this.name,"is going to be riding a",this.bike.name, "\n\nGreat job looks like your ready to race!\n\nNow that you've got your character and your bike let's give our character a new instruction. This time type in your character's name and .race() *Like this: oldLady.race() **\n\nOnce you've done that someone will be selected to race against you.\n\nIf you've got everything typed in and your ready to go just click enter!");
   };
   this.race = function(){
-    var racer = characters[Math.floor(Math.random()*4)];
-    var terrain = environments[Math.floor(Math.random()*4)];
-    racer.ride();
-    console.log(`it looks like ${this.name} on a ${this.bike.name} will be racing against ${racer.name} on a ${racer.bike.name} on the ${terrain.type}`);
+    var racer = randomizer(characters);
+    var terrain = randomizer(environments);
+    // racer.ride();
+    var racerBike = randomizer(bikes);
+    console.log(`it looks like ${this.name} on a ${this.bike.name} will be racing against ${racer.name} on a ${racerBike.name} on the ${terrain.type}`);
 
-    if (this.bike.weight + this.speed + this.energy > racer.bike.weight + racer.speed + racer.energy){
+    if (this.bike.weight + this.speed + this.energy > racerBike.weight + racer.speed + racer.energy){
       return this.name + " wins the race";
     } else {
       return racer.name + " wins the race";
@@ -45,10 +46,15 @@ var pro = new Character({name: "pro", speed: 60, energy: 60});
 var hipster = new Character({name: "hipster", speed: 40, energy: 50});
 var littleKid = new Character({name: "littleKid", speed: 15, energy: 100});
 var characters = [oldLady,pro,hipster,littleKid];
+var randomizer = function(param){
+  var result = param[Math.floor(Math.random()*param.length)];
+  // return result.name || result.type;
+  return result;
+};
 // Bikes
 var trackBike = new Bike({name: "track bike", weight: 5, adaptability: "low"});
-var hybrid = new Bike({name: "hybrid", weight: 12, adaptability: "high"});
-var mountain = new Bike({name: "mountain bike", weight: 20, adaptability: "low"});
+var hybrid = new Bike({name: "hybrid", weight: 10, adaptability: "high"});
+var mountain = new Bike({name: "mountain bike", weight: 1, adaptability: "low"});
 var road = new Bike({name: "road bike", weight: 9, adaptability: "moderate"});
 var bikes = [trackBike,hybrid,mountain,road];
 // Environments
